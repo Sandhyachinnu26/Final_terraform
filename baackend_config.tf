cc19f12f308fa6cf486771190040110ab9a69d77
Sandhyachinnu26 <sandhyachinnu26@gmail.com>
2025-03-18 17:21:53 +0530

    Update baackend_config.tf

--- baackend_config.tf
+++ baackend_config.tf
@@ -1,8 +1,8 @@
-terraform {
+/* terraform {
   backend "s3" {
     bucket         = "batch26terraformbatch26"
     key            = "terraform.tfstate"
     region         = "us-east-1"
     dynamodb_table = "terraform-lock"
   }
-}
+}  */
