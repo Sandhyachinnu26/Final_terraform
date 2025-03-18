bc51e1300c8205c5ce3646f4b863b4ec1dba7d31
Rakesh-k-ops <rakeshkdevops001@gmail.com>
2025-03-18 10:34:38 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -56,15 +56,16 @@ resource "aws_dynamodb_table" "terraform_lock" {
 }
 
 # Terraform Backend Configuration
+
 terraform {
   backend "s3" {
-    bucket         = "batch1terraformbatch1"  # Match the S3 bucket name above
-    key            = "terraform/statefile.tfstate"  # Path in S3
-    region         = "us-east-1"
-    dynamodb_table = "terraform-lock"
-    encrypt        = true
+    bucket      = "batch1terraformbatch1"
+    key         = "terraform/statefile.tfstate"
+    region      = "us-east-1"
+    use_lockfile = true  # Replace dynamodb_table with this
+    encrypt     = true
   }
-} 
+}
 
 
 resource "aws_instance" "sonarqube" {
