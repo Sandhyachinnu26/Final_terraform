8ccd0fa216965f2abbae7fc853cd963b6ff18416
Rakesh-k-ops <rakeshkdevops001@gmail.com>
2025-03-18 11:31:51 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,56 +3,6 @@ provider "aws" {
 }
 
 
-# S3 Bucket for Terraform State Storage
-resource "aws_s3_bucket" "terraform_state" {
-  bucket = "batch1terraformbatch1"
-
-  versioning {
-    enabled = true
-  }
-
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        sse_algorithm = "AES256"
-      }
-    }
-  }
-
-  tags = {
-    Name = "Terraform State Bucket"
-  }
-}
-
-# Block Public Access to S3 Bucket
-resource "aws_s3_bucket_public_access_block" "terraform_state_block" {
-  bucket = aws_s3_bucket.terraform_state.id
-
-  block_public_acls       = true
-  block_public_policy     = true
-  ignore_public_acls      = true
-  restrict_public_buckets = true
-}
-
-# DynamoDB Table for State Locking
-resource "aws_dynamodb_table" "terraform_lock" {
-  name         = "terraform-lock"
-  billing_mode = "PAY_PER_REQUEST"
-  
-  hash_key = "LockID"
-
-  attribute {
-    name = "LockID"
-    type = "S"
-  }
-
-  tags = {
-    Name = "Terraform Lock Table"
-  }
-}
-
-
-
 
 
 # Fetch existing S3 bucket if it exists
