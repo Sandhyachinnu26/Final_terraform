33a57a932e4d25751d7ae8daee4931dde186ca96
Sandhyachinnu26 <sandhyachinnu26@gmail.com>
2025-03-18 15:46:48 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -4,6 +4,7 @@ terraform {
     key            = "terraform/statefile.tfstate"
     region         = "us-east-1"
     encrypt        = true
+    use_lock_table = true
     dynamodb_table = "terraform-lock"
   }
 }
